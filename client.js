a1be84ee9875d9ab5e64cae7cf18cfce5a47546e
Christopher Bull <bullchris2@gmail.com>
2014-02-27 21:20:08 +1300

    front end theory

--- client.js
+++ client.js
@@ -9,8 +9,39 @@ var output = through(function(data){
 output.pipe(ws)
 
 setInterval(function(){
-  output.write([144, 60, 127])
-  setTimeout(function(){
-    output.write([144, 60, 0])
-  }, 100)
-}, 2000)
+  output.write(current)
+}, (1000/60))
+
+var scrollCallback = function(event){
+  current.y = event.pageY/outerHeight
+  current.z = event.pageX/outerWidth
+};
+
+var tiltCallback = function(event){
+  window.leftRight = prepareDegrees(event.gamma);
+  window.frontBack = prepareDegrees(event.beta);
+};
+
+function prepareDegrees(val) {
+  var ret = 0;
+  if(Math.min(val, -90) < -90){
+    ret = -90;
+  } else if (Math.max(val, 90) > 90) {
+    ret = 90;
+  } else {
+    ret = val;
+  }
+
+  return (ret + 90)/360;
+}
+
+document.addEventListener('touchmove', scrollCallback);
+
+window.addEventListener('deviceorientation', tiltCallback);
+
+window.current = {
+  x: 0,
+  y: 0,
+  leftRight: 0,
+  frontBack: 0
+}
